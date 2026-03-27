e0ac84eda7b334f6abe6ca15f33fba38e8b199db
infostracaganass <info.stracaganass@gmail.com>
2026-03-27 15:06:05 +0100

    Update StracaganassWebApp.tsx

--- components/StracaganassWebApp.tsx
+++ components/StracaganassWebApp.tsx
@@ -851,10 +851,10 @@ const login = async () => {
   {`
     @keyframes stracappSponsorScroll {
       0% {
-        transform: translateX(0);
+        transform: translate3d(0, 0, 0);
       }
       100% {
-        transform: translateX(-50%);
+        transform: translate3d(-50%, 0, 0);
       }
     }
   `}
