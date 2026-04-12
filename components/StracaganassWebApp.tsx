f10e36c7a9e9d7b804410f3c68995ae0499c79e4
infostracaganass <info.stracaganass@gmail.com>
2026-04-12 09:10:20 +0200

    Update StracaganassWebApp.tsx

--- components/StracaganassWebApp.tsx
+++ components/StracaganassWebApp.tsx
@@ -2420,36 +2420,36 @@ background: visibleEvents[0]?.id === item.id ? "#eff6ff" : "white",
   </Button>
 
   <button
-    onClick={() =>
-      setAttendanceResponsesOpen((prev) => ({
-        ...prev,
-        [event.id]: !prev[event.id],
-      }))
-    }
-    style={{
-      background: "transparent",
-      border: "none",
-      padding: 0,
-      cursor: "pointer",
-      fontSize: 13,
-      fontWeight: 600,
-      color: "#e2e8f0",
-      display: "flex",
-      alignItems: "center",
-      gap: 4,
-      justifyContent: "flex-start",
-    }}
-  >
-    {attendanceResponsesOpen[event.id] ? (
-      <>
-        <ChevronUp size={14} /> Nascondi risposte
-      </>
-    ) : (
-      <>
-        <ChevronDown size={14} /> Mostra risposte
-      </>
-    )}
-  </button>
+  onClick={() =>
+    setAttendanceResponsesOpen((prev) => ({
+      ...prev,
+      [event.id]: !prev[event.id],
+    }))
+  }
+  style={{
+    background: "transparent",
+    border: "none",
+    padding: 0,
+    cursor: "pointer",
+    fontSize: 13,
+    fontWeight: 600,
+    color: "#e2e8f0",
+    display: "flex",
+    alignItems: "center",
+    gap: 4,
+    justifyContent: "flex-start",
+  }}
+>
+  {attendanceResponsesOpen[event.id] ? (
+    <>
+      <ChevronUp size={14} /> Nascondi risposte ({responses.length})
+    </>
+  ) : (
+    <>
+      <ChevronDown size={14} /> Mostra risposte ({responses.length})
+    </>
+  )}
+</button>
 </div>
 
               {responsesOpen ? (
